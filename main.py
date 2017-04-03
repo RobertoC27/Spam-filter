6409e4e70beff8ae0089a6b7312c09a8b4d75354
RobertoC27 <chi13027@uvg.edu.gt>
2017-04-03 13:27:34 -0600

    minor style changes

--- main.py
+++ main.py
@@ -16,16 +16,17 @@ def runMe():
     for line in ccf:
         currently_classifying.append(line)
     ccf.close()
+    results = []
     while seguir != 'n':
-        cambiar = raw_input("desea cambiar el k actual {}? (y|n)".format(k))
+        cambiar = raw_input("desea cambiar el k actual {}? (y|n)\n".format(k))
 
         if cambiar == 'y':
-            k = input("ingrese el nuevo valor de K")
+            k = input("ingrese el nuevo valor de K: ")
             assisting = setUP(k)
 
-        cambiar = raw_input("desea cambiar el archivo actual {}? (y|n)".format(file_name))
+        cambiar = raw_input("desea cambiar el archivo actual {}? (y|n)\n".format(file_name))
         if cambiar == 'y':
-            file_name = raw_input("ingrese el nuevo nombre de archivo")
+            file_name = raw_input("ingrese el nuevo nombre de archivo\n")
             currently_classifying = []
             ccf = open(file_name, "r")
             for line in ccf:
@@ -33,14 +34,26 @@ def runMe():
             ccf.close()
 
         efectividad = 0
+        results = []
         for mail in currently_classifying:
             res = calcProb(mail, k, assisting[0], assisting[1], assisting[2])
-            if mail.startswith("ham") and res[0] > res[1]:
-                efectividad += 1
-            elif mail.startswith("spam") and res[1] > res[0]:
-                efectividad += 1
+            if res[0] > res[1]:
+                if mail.startswith("ham"):
+                    efectividad += 1
+                else:
+                    results.append("ham\t{}".format(mail))
+
+            if res[1] > res[0]:
+                if mail.startswith("spam"):
+                    efectividad += 1
+                else:
+                    results.append("spam\t{}".format(mail))
+
+        rf = open("results.txt", "w")
+        rf.writelines(results)
+        rf.close()
         efectividad = efectividad / float(len(currently_classifying))
-        print "la efectividad de deteccion para {}, fue del {}%".format(file_name, round(efectividad * 100, 3))
+        print "la efectividad de deteccion para {}, fue del {}% con k = {}".format(file_name, round(efectividad * 100, 3), k)
         seguir = raw_input("volver a ejecutar? (y|n)\n")
 
 runMe()
